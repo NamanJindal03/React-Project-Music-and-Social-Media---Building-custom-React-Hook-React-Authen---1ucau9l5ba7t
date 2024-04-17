b16d27ae4d109d1b15d1956cf7037dc1ecccd3ec
Naman Jindal <namanjindal113@gmail.com>
2024-04-17 23:04:22 +0530

    search done

--- components/HomeComponents/MusicList.js
+++ components/HomeComponents/MusicList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import constants from '../../constants.js';
 import Card from '../Common/Card.js';
+import { useRouter } from 'next/router'
 
 const {BASE_API_PATH, PROJECT_ID, APP_TYPE} = constants;
 
 export default function MusicList() {
   const [musicList, setMusicList] = useState([]);
+  const router = useRouter();
+  console.log(router.query)
+  const musicQuery = router.query.music;
 
   async function getMusicList(){
     try{
@@ -22,12 +26,44 @@ export default function MusicList() {
     catch(err){
         console.log(err)
     }
-    
-}
+  }
+
+  async function getSearchedMusic(){
+    try{
+      const sample = {title: musicQuery}
+        const response = await fetch(`${BASE_API_PATH}/api/v1/music/song?search=${JSON.stringify(sample)}`, {
+            method: 'GET',
+            headers: {
+                "Content-type": "application/json",
+                "projectId": PROJECT_ID
+            },
+        });
+        if(!response.ok){
+          setMusicList([]);
+          return;
+        }
+        const data = await response.json();
+        console.log(data);
+        if(data?.data){
+          setMusicList(data.data);
+        }
+    }
+    catch(err){
+        console.log(err);
+        setMusicList([]);
+
+    }
+  }
 
   useEffect(()=>{
-    getMusicList();
-  },[])
+    console.log(musicQuery)
+    if(musicQuery){
+      getSearchedMusic()
+    }
+    else{
+      getMusicList();
+    }
+  },[musicQuery])
 
 
   return (
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useAuth } from '@/context/AuthContext'
 import css from '@/styles/Navbar.module.css'
+import { useRouter } from 'next/router'
+import useDebounce from '@/hooks/useDebounce'
 
 export default function NavBar() {
     const {token, name} = useAuth();
+    const router = useRouter();
+    const musicQuery = router.query.music;
+
+    const [musicSearch, setMusicSearch] = useState(musicQuery);
+
+    const isRoot = router.pathname === '/';
+
+    const musicQuerySearch = useDebounce(musicSearch, 1000)
+
+    function handleSearch(musicSearchValue){
+        // router.push({
+        //     pathname: '/',
+        //     query: {music: musicSearch}
+        // })
+        setMusicSearch(musicSearchValue)
+    }
+
+    useEffect(()=>{
+        console.log('called')
+        router.push({
+            pathname: '/',
+            query: {music: musicQuerySearch}
+        })
+    },[musicQuerySearch])
+
   return (
     <div className={`${css.navbar} flex ${css.container}`}> 
         <div className='brand-img'>Brand Image</div>
@@ -15,10 +42,11 @@ export default function NavBar() {
                 <li><Link href={'/library'}>Library</Link></li>
             </ul>
         </nav>
-        <div className='input-bar'>
-            <input type="text" />
-        </div>
-
+        {isRoot &&
+            <div className='input-bar'>
+                <input type="text" value={musicSearch} onChange={(e)=>handleSearch(e.target.value)}/>
+            </div>
+        }
         <div>
             <Link href={'/login'}><button>human</button></Link>
             {token && <span>{name}</span>}
